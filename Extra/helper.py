0c56dbe9dc8d35f3d5601abbec950457e0be1540
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-05-23 22:40:54 +0530

    Create helper.py

---
+++ Extra/helper.py
@@ -0,0 +1,13 @@
+
+
+plt.scatter(data.comubstion,data.emission)
+
+from sklearn.metrics import r2_score,mean_squared_error
+
+print("R2 score",r2_score(y_test,y_pred))
+print("RMSE",np.sqrt(mean_squared_error(y_test,y_pred)))
+
+plt.plot(X_test,L.predict(X_test),label="Modal")
+plt.scatter(X_train,y_train,label="data", color="r")
+plt.legend()
+plt.show()
