26881d19b0cca314811dfdb5099378ed940e5af1
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-05-29 00:50:55 +0530

    Update helper.py

--- Extra/helper.py
+++ Extra/helper.py
@@ -19,3 +19,8 @@ data.column-name.isnull.any()
 
 sns.distplot(chess_df['turns'])
 sns.boxplot(chess_df['turns'])
+
+chess_df=chess_df[['rated', 'created_at', 'last_move_at', 'turns', 'victory_status',
+       'winner', 'increment_code', 'white_rating',
+       'black_rating', 'moves', 'opening_eco','opening_name','opening_ply']]
+chess_df_subset
