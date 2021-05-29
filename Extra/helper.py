eb27d7df87b62ebce515364bc899318a6edea853
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-05-30 01:02:53 +0530

    Update helper.py

--- Extra/helper.py
+++ Extra/helper.py
@@ -26,3 +26,39 @@ chess_df=chess_df[['rated', 'created_at', 'last_move_at', 'turns', 'victory_stat
 chess_df_subset
 
 chess_df.dtypes
+
+#plot a scatterplot of sugars and ratings variables
+cereals_df.plot.scatter(x='sugars',y='rating',c='green', figsize=(12,7))
+plt.xlabel("Sugars (in grams)")
+plt.ylabel("Rating")
+plt.title('Sugars Amount vs Rating')
+
+
+#compute correlation table to analyze variables relationship
+corr=cereals_df.corr().round(2)
+corr
+
+#create a heatmap to demonstrate the correlation magnitude among the variables
+sns.set(font_scale=1.2)
+fig, ax = plt.subplots(figsize=(12,8))
+sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, annot=True, annot_kws={'fontsize':13},
+            fmt=".2f", cmap='PiYG', center=0) #annot argument writes the data value into each cell 
+                                                #fmt parameter add text to the heatmap cell and formats the cell values
+       
+      
+fig, ax = plt.subplots(nrows=2, ncols=5, figsize=(18,8))
+i=0
+
+#adjust the space between main title and subplots
+fig.subplots_adjust(top=0.90)
+
+for row in ax:
+    for col in row:
+        n=cereals_df.columns[i]
+        i=i+1
+        plt.subplot(2,5,i)
+        plt.hist(cereals_df[n], color='mediumturquoise')
+        plt.xlabel(n, fontsize=13)
+               
+plt.suptitle("Cereals Components Distribution", size=15)   
+plt.show()
